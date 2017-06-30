849469b6b89b727c2924ac020c75eb0ae763eeee
gabriel carballude <gabrielcarballude@gmail.com>
2017-06-30 16:01:22 -0400

    Esta todo, se abandono el upload de archivos debido a problemas, sera para la siguiente persona

--- flaskAsynchronousServer.py
+++ flaskAsynchronousServer.py
@@ -12,6 +12,7 @@ app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
 
+# When a client connects we must emit the match initialization and current moves if they exist
 @socketio.on('client_connect')
 def status_match():
     global init_info
@@ -20,12 +21,17 @@ def status_match():
     print('Someone got connected')
 
     if len(init_info) != 0:
+        print('We are trying to send initial match data (Async Server -> Client):')
+        print(init_info)
         socketio.emit('init_match', init_info)
 
     if len(status_info) != 0:
+        print('We are trying to send match status (Async Server -> Client):')
+        print(init_info)
         socketio.emit('status_match', status_info)
 
 
+# When we recieve from the sync server that a match has started we must reset a couple of fields and broadcast the event
 @socketio.on('init_match')
 def init_match(data):
     global init_info
@@ -33,16 +39,20 @@ def init_match(data):
     init_info = data
     status_info = []
 
-    print('We got info for a new match!')
+    print('A new match has begun')
+    print('We are trying to send initial match data (Async Server -> Client):')
     print(data)
     socketio.emit('init_match', data)
-    print('Data sent to others')
 
+
+# When we recieve a new move from the sync server we must append it to the total moves made list and broadcast the move
 @socketio.on('update_match')
 def update_match(data):
     global status_info
     status_info.append(data)
 
+    print('We are trying to send match update (Async Server -> Client):')
+    print(data)
     socketio.emit('update_match', data)
 
 
---
+++ plugins/serial_dicer5.py
@@ -0,0 +1,30 @@
+from helper import print_dice, print_dice_val, print_scoresheets
+import string
+
+def name():
+    return "SERIAL 5"
+
+
+# this plugin will play serially to the next available free space
+# in its corresponding scoresheet
+
+def play(roll_no, dice, bonus, players, scoresheets):
+    # where
+    i = 0
+    for ss in scoresheets:
+        my_ss = ss[name()]
+        if '4' not in my_ss:
+            return [], '4', i
+        elif '5' not in my_ss:
+            return [], '5', i
+        elif '6' not in my_ss:
+            return [], '6', i
+        elif 'ESCALERA' not in my_ss:
+            return [], 'ESCALERA', i
+        elif 'FULLHOUSE' not in my_ss:
+            return [], 'FULLHOUSE', i
+        elif 'POKER' not in my_ss:
+            return [], 'POKER', i
+        elif 'GENERALA' not in my_ss:
+            return [], 'GENERALA', i
+        i = i + 1
